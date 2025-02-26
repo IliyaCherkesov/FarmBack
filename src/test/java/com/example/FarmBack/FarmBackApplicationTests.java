0b5931f1ff5f9101366dcfc19e74e90b769ebfff
SampleFox <61387491+IliyaCherkesov@users.noreply.github.com>
2025-02-26 22:01:40 +0200

    first commit

---
+++ src/test/java/com/example/FarmBack/FarmBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FarmBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
