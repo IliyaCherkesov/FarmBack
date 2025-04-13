9f468ab5b0954fb319146b15805ccf17294120e7
SampleFox <61387491+IliyaCherkesov@users.noreply.github.com>
2025-04-13 21:42:22 +0300

    implemented swagger

--- src/main/java/com/FarmBack/Model/User.java
+++ src/main/java/com/FarmBack/Model/User.java
@@ -1,34 +1,44 @@
 package com.FarmBack.Model;
 
-import jakarta.persistence.*;
 
-import java.sql.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User {
+
     @Id
     String id;
+    public String getId() {return id;}
 
-    public String getId() { return id; }
+    String birthday;
+    public String getBirthday() {
+        return birthday;
+    }
 
     String name;
-
-    public String getName() { return name; }
-
-    Date birthday;
-
-    public Date getBirthday() { return birthday; }
+    public String getName() {
+        return name;
+    }
 
     String email;
 
-    public String getEmail() { return email; }
+    public String getEmail() {
+        return email;
+    }
 
     String phone;
 
-    public String getPhone() { return phone; }
+    public String getPhone() {
+        return phone;
+    }
 
     String role_id;
 
-    public String getRole_id() { return role_id; }
+    public String getRole_id() {
+        return role_id;
+    }
+
 }
--- src/main/java/com/FarmBack/Repositories/UserRepository.java
+++ src/main/java/com/FarmBack/Repositories/UserRepository.java
@@ -4,8 +4,7 @@ import com.FarmBack.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
 }
